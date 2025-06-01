4d651d92fa03b1fb95708da8523ad5d90ce8cc0c
Agawaen <agawainex@gmail.com>
2025-06-01 14:05:55 -0700

    organized global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,11 @@
 	let { children } = $props();
 </script>
 
-<div>
+<div class="page">
     <title>
         Ren and the Stimpies
     </title>
-    
+
     <Navbar/>
 
     {@render children()}
@@ -31,33 +31,37 @@
         src: url('$lib/fonts/Perfect DOS VGA 437.ttf');
     }
 
-    :global(body) {
-        font-family: 'Perfect DOS VGA 437';
-        font-size: 1.5em;
+    :global {
+        body {
+            font-family: 'Perfect DOS VGA 437';
+            font-size: 1.5em;
+            color: #eee;
+            background-color: #000;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            font-family: 'Alagard';
+            font-size: 1em;
+        }
+
+        h1 {
+            font-size: 3em;
+            margin: .5em 0 .25em 0;
+            text-align: center;
+        }
+
+        h2 {
+            font-size: 2em;
+            margin: .4em 0 0 0;
+        }
+
+        h3 {
+            font-size: 1.5em;
+            margin: .3em 0 0 0;
+        }
     }
 
-    :global(h1, h2, h3, h4, h5, h6) {
-        font-family: 'Alagard';
-        font-size: 1em;
-    }
-
-    :global(h1) {
-        font-size: 3em;
-        margin: .5em 0 .25em 0;
-        text-align: center;
-    }
-
-    :global(h2) {
-        font-size: 2em;
-        margin: .4em 0 0 0;
-    }
-
-    :global(h3) {
-        font-size: 1.5em;
-        margin: .3em 0 0 0;
-    }
-
-    div {
+    .page {
         display: flex;
         flex-direction: column;
         align-items: center;
